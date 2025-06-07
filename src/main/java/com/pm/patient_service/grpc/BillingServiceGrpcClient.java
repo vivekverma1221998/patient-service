c60c507406bf6a4b42fe4bfc91797156c77d0561
vivekverma1221998 <vivekverma1221998@gmail.com>
2025-06-08 01:09:55 +0530

    fix: adding grpc

--- src/main/java/com/pm/patient_service/grpc/BillingServiceGrpcClient.java
+++ src/main/java/com/pm/patient_service/grpc/BillingServiceGrpcClient.java
@@ -1,7 +1,10 @@
 package com.pm.patient_service.grpc;
 
+import billing.BillingRequest;
+import billing.BillingResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,7 +24,27 @@ public class BillingServiceGrpcClient {
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort).usePlaintext().build();
 
-        this.blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void createBillingAccount(String patientId, String name , String email) {
+        BillingRequest request = BillingRequest.newBuilder()
+                .setPatientId(patientId)
+                .setName(name)
+                .setEmail(email).build();
+
+        BillingResponse response = null;
+
+        try {
+            response = blockingStub.createBillingAccount(request);
+            log.info("Billing response: {}", response);
+        } catch (StatusRuntimeException e) {
+            log.error("gRPC call failed: {}", e.getStatus(), e);
+        }
+        
+
+        log.info("Received response from Billing service via GRPC: {}", response);
+
 
     }
 }
--- src/main/java/com/pm/patient_service/service/PatientService.java
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,6 +4,7 @@ import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.exception.PatientNotFoundException;
+import com.pm.patient_service.grpc.BillingServiceGrpcClient;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -17,10 +18,12 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getPatients() {
@@ -39,6 +42,9 @@ public class PatientService {
         }
 
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+                newPatient.getName(), newPatient.getEmail());
         return PatientMapper.toDTO(newPatient);
     }
 
